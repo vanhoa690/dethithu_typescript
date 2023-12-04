2535def3e205e3fd955c37286e59fd420b8362d6
vanhoa690 <vanhoa690@gmail.com>
2023-12-04 08:42:08 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="my-4 text-center text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Đề thi thử Typescript - WEB 502
+      </h1>
+    </>
+  );
+}
+
+export default App;
